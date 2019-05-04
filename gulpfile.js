76365331724999825b5a5857696b557e69035abf
KaitlynBro <kaitlynbrouwers@gmail.com>
2019-05-04 14:29:15 -0400

    fixing mobile error

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,49 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
-const concat = require('gulp-concat');
-const babel = require('gulp-babel');
-const autoprefixer = require('gulp-autoprefixer');
-const browserSync = require('browser-sync').create();
+const babel = require('babelify');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const browserSync = require('browser-sync');
 const reload = browserSync.reload;
-
-gulp.task('browser-sync', () => {
-  browserSync.init({
-    server: '.'  
-  })
-});
+const notify = require('gulp-notify');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const concat = require('gulp-concat');
 
 gulp.task('styles', () => {
-  return gulp.src('./dev/styles/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-    .pipe(concat('style.css'))
-    .pipe(gulp.dest('./public/styles'))
-    .pipe(reload({stream: true}));
+	return gulp.src('./dev/styles/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(concat('style.css'))
+		.pipe(gulp.dest('./public/styles'))
 });
 
-gulp.task('scripts', () => {
-  return gulp.src('./dev/scripts/app.js')
-    .pipe(babel({
-      presets: ['es2015']
-    }))
-    .pipe(gulp.dest('./public/scripts'))
-    .pipe(reload({stream: true}));
+gulp.task('js', () => {
+	return browserify('dev/scripts/app.js', {debug: true})
+		.transform('babelify', {
+			sourceMaps: true,
+			presets: ['env','react']
+		})
+		.bundle()
+		.on('error',notify.onError({
+			message: "Error: <%= error.message %>",
+			title: 'Error in JS 💀'
+		}))
+		.pipe(source('app.js'))
+		.pipe(buffer())
+		.pipe(gulp.dest('public/scripts'))
+		.pipe(reload({stream:true}));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('./dev/scripts/*.js', ['scripts']);
-  gulp.watch('./dev/styles/*.scss', ['styles']);
-  gulp.watch('*.html', reload);
+gulp.task('bs', () => {
+	return browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	});
 });
 
-gulp.task('default', ['browser-sync','styles', 'scripts', 'watch']);
+gulp.task('default', ['bs','js','styles'], () => {
+	gulp.watch('dev/**/*.js',['js']);
+	gulp.watch('dev/**/*.scss',['styles']);
+	gulp.watch('./public/styles/style.css',reload);
+});
